b5b8416d7abf135ef83e36b46f24eaee4647cc75
Luke Weston <lukeweston@Lukes-MBP.attlocal.net>
2023-06-19 21:50:59 -0400

    backing up

--- src/features/gameplay/card/card.tsx
+++ src/features/gameplay/card/card.tsx
@@ -131,12 +131,13 @@ export default function Card(cardProps: CardProps) {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </svg>
+            <h6 className="neon-text">{cardProps.cardStats.health}</h6>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="none"
+              fill=""
               viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke="currentColor"
+              stroke="#F2BFD7"
               className="h-6 w-6"
             >
               <path
@@ -150,8 +151,37 @@ export default function Card(cardProps: CardProps) {
                 d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z"
               />
             </svg>
-
-            <h6 className="neon-text">{cardProps.cardStats.health}</h6>
+            <h6 className="neon-text">{cardProps.cardStats.damage}</h6>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#F2BFD7"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+              />
+            </svg>
+            <h6 className="neon-text">{cardProps.cardStats.energy}</h6>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#F2BFD7"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <h6 className="neon-text">{cardProps.cardStats.cost}</h6>
           </div>
         </div>
         <div className="absolute bottom-0 left-0 self-end ">
